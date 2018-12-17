9111ce420bab619d3928a27c0e9334370b8f0527
Raul Pavani <40846165+RaulPavani@users.noreply.github.com>
2018-12-17 20:37:08 -0200

    Add files via upload

---
+++ GravityCarVR Game/Assets/Script/Sensores.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensores : MonoBehaviour {
+
+
+    [SerializeField]
+    private float m_MaxRange;
+    private float m_Range;
+
+    [SerializeField]
+    private LayerMask m_LayerMask;
+    [SerializeField]
+    private Color m_NormalRayColor, m_CollisionRayColor;
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        //transform.TransformDirection
+        if (Physics.Raycast(transform.position, (Vector3.forward), out hit, m_Range, m_LayerMask))
+        {
+            m_Range = hit.distance;
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * m_Range, m_CollisionRayColor);
+        }
+        else
+        {
+            m_Range = m_MaxRange;
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * m_Range, m_NormalRayColor);
+        }
+
+
+    }
+
+}
+
